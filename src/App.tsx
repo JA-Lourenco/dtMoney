51e06718b4f709482003ed886c89920f007a8174
JA-Lourenco <ja.lourenco@outlook.com>
2022-04-07 13:56:04 -0300

    Aplicando Context API

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
 import Modal from 'react-modal'
+import { useState } from 'react';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './TransactionsContext';
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root')
@@ -19,7 +20,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
 
       <Dashboard />
@@ -30,6 +31,6 @@ export function App() {
       />
       
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,14 +1,12 @@
 import { Summary } from '../Summary'
-import { TrasanctionsTable } from '../TransactionsTable'
+import { TransactionsTable } from '../TransactionsTable'
 import { Container } from './styles'
-import {  } from './styles'
-
 
 export function Dashboard() {
     return (
         <Container>
             <Summary></Summary>
-            <TrasanctionsTable></TrasanctionsTable>
+            <TransactionsTable></TransactionsTable>
         </Container>
     )
 }
